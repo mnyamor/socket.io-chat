e90c15d3effca20d127116059da5bdaf0d3220f6
Mary Nyamor <mnyamor@gmail.com>
2016-03-30 23:38:40 +0300

    test connection

--- index.js
+++ index.js
@@ -5,9 +5,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+//initialize a new instance of socket.io
+var io = require('socket.io')(http);
+
 // define a route handler / that gets called when we hit our website home.
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    //res.sendFile(__dirname + '/index.html');
+    res.sendfile('index.html');
+
+});
+
+//listen on the connection event for incoming sockets, and log it to the console
+io.on('connection', function(socket){
+    console.log('a user connected');
 });
 
 http.listen(3000, function () {
